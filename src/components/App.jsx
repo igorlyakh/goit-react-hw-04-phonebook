417d74c710e02a0dcaf56c0e80c832f0082a3d56
Igor Liakh <igorlyah060@gmail.com>
2023-11-25 15:55:24 +0200

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import AddContactForm from './AddContactForm';
+import ContactsList from './ContactsList';
+import FilterField from './FilterField';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onNameSubmit = person => {
+    const checkContact = this.state.contacts.some(
+      contact => contact.name.toLowerCase() === person.name.toLowerCase()
+    );
+    if (checkContact) {
+      alert(`${person.name} is already in contacts.`);
+      return;
+    }
+    this.setState(prev => {
+      return {
+        contacts: [...prev.contacts, { ...person, id: nanoid() }],
+      };
+    });
+  };
+
+  onDelete = id => {
+    this.setState(prev => {
+      return {
+        contacts: prev.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  onFilter = value => {
+    this.setState({
+      filter: value,
+    });
+  };
+
+  getFilteredContacts = () => {
+    return this.state.contacts.filter(contact => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase());
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const visibleContacts = this.getFilteredContacts();
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <AddContactForm onNameSubmit={this.onNameSubmit} />
+        <FilterField contactFilter={filter} onFilter={this.onFilter} />
+        {contacts.length > 0 && (
+          <ContactsList contacts={visibleContacts} onDelete={this.onDelete} />
+        )}
+      </>
+    );
+  }
+}
